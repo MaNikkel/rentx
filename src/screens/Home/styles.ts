4007310ca1a3788bc04502152609bb8c3524434c
Mathias Nikkel <mathiasnikkel@gmail.com>
2021-12-21 22:14:17 -0300

    initial commit

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  
+  background-color: ${({ theme }) => theme.colors.main}
+`
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.secondary_500};
+`
+
+
