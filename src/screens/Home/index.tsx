51d972cd768a95a15d5a038ea3c12ec69cd5f4ef
Mathias Nikkel <mathiasnikkel@gmail.com>
2021-12-22 13:55:03 -0300

    assets and firts page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,53 @@
 import React from 'react'
 
-import {
-    Container, Title
-} from './styles'
+import Logo from '../../assets/logo.svg'
+
+import {CarList, Container, Header, HeaderContent, TotalCars} from './styles'
+import {StatusBar} from "react-native";
+import {RFValue} from "react-native-responsive-fontsize";
+import {Car} from "../../components/Car";
 
 export function Home() {
-    return (
-        <Container>
-            <Title>Home</Title>
-        </Container>
-    )
+  return (
+    <Container>
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+
+      <Header>
+        <HeaderContent>
+          <Logo
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <CarList
+        data={[1, 2, 3]}
+        keyExtractor={item => String(item)}
+        renderItem={(item) => (
+          <Car
+            data={{
+              rent: {
+                period: 'adf',
+                price: 10
+              },
+              thumbnail: '',
+              name: 'asd',
+              brand: 'asf',
+            }}
+          />
+        )}
+
+      />
+
+
+    </Container>
+  )
 }
